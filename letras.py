17a9675e43ebe634e63099fcc66b5c95adcca222
Maria Fernanda Bonifácio Gonçalves <160647200+maferbonifacio@users.noreply.github.com>
2026-03-30 22:48:02 -0300

    Add letra position checker for letters a, b, c

---
+++ letras.py
@@ -0,0 +1,9 @@
+letra = str(input('Digite uma letra: '))
+if letra.lower() == 'a':
+    print('Posição: 1')
+else:
+    if letra.lower() == 'b':
+         print('Posição: 2')
+    else:
+        if letra.lower() == 'c':
+          print('Posição: 3')    
